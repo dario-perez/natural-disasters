b7fe950ac9a04ae9b89149526861307ca60b96b7
darionic30 <dario.nic30@gmail.com>
2020-09-28 01:48:37 -0300

    Updates

---
+++ precache-manifest.f80f52b7e0f336f6f52aa71a5f99d9bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452f702690f70874aa786ce45715ed91",
+    "url": "/natural-disasters/index.html"
+  },
+  {
+    "revision": "eca0ddeca07e70d05bca",
+    "url": "/natural-disasters/static/css/main.5301531b.chunk.css"
+  },
+  {
+    "revision": "a33986a1a88ff586302d",
+    "url": "/natural-disasters/static/js/2.db4c8980.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/natural-disasters/static/js/2.db4c8980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eca0ddeca07e70d05bca",
+    "url": "/natural-disasters/static/js/main.5853eef8.chunk.js"
+  },
+  {
+    "revision": "84c9c291289ce8800474",
+    "url": "/natural-disasters/static/js/runtime-main.ca9bc3af.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/natural-disasters/precache-manifest.f80f52b7e0f336f6f52aa71a5f99d9bb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/natural-disasters/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
