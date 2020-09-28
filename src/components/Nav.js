efba4de00ecbc612bd4c035fb3bbad125a020d05
darionic30 <dario.nic30@gmail.com>
2020-09-28 10:17:24 -0300

    Anchor tags repaired

---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Nav() {
+	return (
+		<nav className="navbar">
+			<ul>
+				<li>
+					<a href="#home">Home</a>
+				</li>
+				<li>
+					<a href="#volcanoes">Volcanoes</a>
+				</li>
+				<li>
+					<a href="#tornadoes">Tornadoes</a>
+				</li>
+				<li>
+					<a href="#wildfire">Wildfire</a>
+				</li>
+			</ul>
+		</nav>
+	);
+}
---
+++ src/components/Sections.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default function Sections() {
+	return (
+		<div className="sections">
+			<section id="home">
+				<h1>Natural disasters</h1>
+				<p>Keep yourself and your loved ones safe during an unexpected disaster.</p>
+			</section>
+			<section id="volcanoes">
+				<h1>Volcanoes</h1>
+				<p>
+					Volcanoes can produce ash, toxic gases, flash floods of hot water and debris called lahars, lava
+					flows, and fast-moving flows of hot gases and debris called pyroclastic flows. Some dangers from
+					volcanoes can be predicted ahead of time while others may occur with little or no notice after an
+					eruption. Each volcano and situation is unique. Learn more about volcanic eruptions and pay
+					attention to warnings from local authorities for the best advice available on specific actions you
+					can take to stay safe.
+				</p>
+			</section>
+			<section id="tornadoes">
+				<h1>Tornadoes</h1>
+				<p>
+					Knowing what to do when you see a tornado, or when you hear a tornado warning, can help protect you
+					and your family. During a tornado, people face hazards from extremely high winds and risk being
+					struck by flying and falling objects. After a tornado, the wreckage left behind poses additional
+					injury risks. Although nothing can be done to prevent tornadoes, there are actions you can take for
+					your health and safety.
+				</p>
+			</section>
+			<section id="wildfire">
+				<h1>Wildfire</h1>
+				<p>
+					More and more people make their homes in areas that are prone to wildfires. You can take steps to be
+					ready for a wildfire and prepare your home and landscaping to reduce your risk. Learn how to protect
+					yourself and your family from a wildfire, evacuate safely during a wildfire, and how to stay healthy
+					when you return home.
+				</p>
+			</section>
+		</div>
+	);
+}
